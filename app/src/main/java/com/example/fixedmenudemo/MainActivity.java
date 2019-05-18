ed8d4a84b4b61ce3e0a831f7bb2b89edfb6733d9
Izzy <izzy@Kamarudins-iMac.local>
2019-05-18 23:16:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fixedmenudemo/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.fixedmenudemo;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.home_nav:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.cart_nav:
+                            selectedFragment = new CartFragment();
+                            break;
+                        case R.id.prof_nav:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+
+                }
+            };
+}
